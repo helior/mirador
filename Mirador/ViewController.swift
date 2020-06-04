fdbf4104d1c2e43fa53eeb75be5e7fc4c3364964
Helior Colorado <me@helior.info>
2020-06-03 23:38:03 -0700

    All I remember from last time

--- Mirador/ViewController.swift
+++ Mirador/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import ContactsUI
 
 class ViewController: UIViewController {
-
+    var contactPicker:CNContactPickerViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.requestAccessToContacts()
+        print("did start")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+extension ViewController {
+    func requestAccessToContacts() {
+//        let store = CNContactStore()
+//
+//        if (CNContactStore.authorizationStatus(for: .contacts) == .notDetermined) {
+//            store.requestAccess(for: .contacts, completionHandler: { (authorized, error) in
+//                print("stuff")
+//            })
+////            store.requestAccess(for: .contacts, completionHandler: {(authorized:Bool, error:Error?) -> Void in
+////                if (authorized) {
+////                    print("Yay, authorized!")
+////                }
+////                else {
+////                    print("Not authorized...")
+////                }
+////            })
+//        }
+//        else if CNContactStore.authorizationStatus(for: .contacts) == .authorized {
+////            print("Already authorized!")
+//        }
+    }
+}
+
+extension ViewController: CNContactPickerDelegate {
+    @IBAction func invokeContactListUI() {
+        // must be persisted in the instance, otherwise the delegate methods aren't invoked.
+        self.contactPicker = CNContactPickerViewController()
+        self.contactPicker.delegate = self
+        self.present(self.contactPicker, animated: true, completion: nil)
+    }
+    
+    func contactPickerDidCancel(_ picker: CNContactPickerViewController) {
+        print("user cancelled.")
+    }
+    
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+        print("Contact selelected!")
+        print("\(contact.identifier)")
+    }
+    
+//    func contactPicker(_ picker: CNContactPickerViewController, didSelect contactProperty: CNContactProperty) {
+//        print("Contact properties selected")
+//    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## Codename: Mirador
+Remember, this was about taking notes about people in your life. Consider using Monica as a back-end for this.
